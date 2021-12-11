972b7da8455f7477988513549fd3f72ce53c52cd
Darius Ratkevičius <darius@ratkevicius.lt>
2021-12-11 10:48:30 +0200

    Moved public dir to /docs to comply with git pages available document roots.

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,6 @@ module.exports = {
     },
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public/dist'),
+        path: path.resolve(__dirname, 'docs/dist'),
     },
 };
